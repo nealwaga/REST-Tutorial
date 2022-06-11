aa5ba9db6ff9a6f9b0e01e9f7a15f7e061eccc0c
nealwaga <waganeal@gmail.com>
2022-06-11 18:00:54 +0300

    Created views & Updated urls

--- REST/urls.py
+++ REST/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from rest_framework.urlpatterns import format_suffix_patterns
+from web_app import views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('employees/', views.EmployeeList.as_view()),
 ]
--- web_app/views.py
+++ web_app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+
+from .models import Employees
+from .serializer import EmployeesSerializer
+
+from rest_framework import APIView #used so the normal views can return API data
+from rest_framework.response import Response #where you get back the status or a particular response(if it's okay it will return 200)
+from rest_framework import status #basically send back the status
+
 
 # Create your views here.
+class EmployeeList(APIView):
+
+    #to return all employees in the model
+    def get(self, request):
+        employess1 = Employees.objects.all() #variable to store all my objects
+        serializer = EmployeesSerializer(employess1, many=True) #taking all objects & converting them to JSON
+        return Response(serializer.data)
+
+
+    #to create a new employee
+    def post(self):
+        pass
