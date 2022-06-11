e35fa1768b2bc9db740ee49294189f14c1020a6f
nealwaga <waganeal@gmail.com>
2022-06-11 18:02:50 +0300

    Fixed bug

--- web_app/views.py
+++ web_app/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import get_object_or_404
 from .models import Employees
 from .serializer import EmployeesSerializer
 
-from rest_framework import APIView #used so the normal views can return API data
+from rest_framework.views import APIView #used so the normal views can return API data
 from rest_framework.response import Response #where you get back the status or a particular response(if it's okay it will return 200)
 from rest_framework import status #basically send back the status
 
